a6aef284ad33db0b084730314ff20d78e67adc8d
Ryan P. Brewster <RyanPBrewster@gmail.com>
2020-06-07 22:17:12 -0700

    allow reverse lookups

--- worker/index.js
+++ worker/index.js
@@ -4,23 +4,48 @@ addEventListener('fetch', (event) => {
 
 async function handleRequest(request) {
   const url = new URL(request.url)
-  const path = url.pathname
-  const { namespace, name } = parsePath(path)
-  if (!namespace) {
+  const parsed = parsePath(url)
+  if (!parsed) {
     return new Response('Invalid namespace', { status: 400 })
   }
+  const { namespace, name } = parsed;
   switch (request.method.toUpperCase()) {
     case 'GET':
-      return name
-        ? redirectToLink(namespace, name, url.origin + '/' + namespace)
-        : listLinks(namespace)
+      if (name) {
+        return redirectToLink(namespace, name, url.origin + '/' + namespace);
+      }
+      const target = request.headers.get('x-golinks-target');
+      if (target) {
+        return lookupByLink(namespace, target);
+      }
+      return keyOverview(namespace);
     case 'PUT':
       return setLink(namespace, name, await request.text())
   }
   return new Response('Not found', { status: 404 })
 }
 
-async function listLinks(namespace) {
+async function redirectToLink(namespace, name, origin) {
+  const link = await golinks.get(encodeKey(namespace, name))
+  return Response.redirect(link ?? origin)
+}
+
+async function setLink(namespace, name, link) {
+  await Promise.all([
+    golinks.put(encodeKey(namespace, name), link),
+    golinks.put(encodeLink(namespace, link, name), true),
+  ]);
+  return new Response(`set link ${namespace}/${name} to ${link}`)
+}
+
+async function lookupByLink(namespace, link) {
+  if (!namespace) throw Error('invalid namespace')
+  const list = await golinks.list({ prefix: encodeLink(namespace, link, '') })
+  const keys = list.keys.map((item) => decodeLink(namespace, link, item.name));
+  return new Response(JSON.stringify(keys), { headers: { 'Content-Type': 'application/json' } })
+}
+
+async function keyOverview(namespace) {
   if (!namespace) throw Error('invalid namespace')
   const list = await golinks.list({ prefix: encodeKey(namespace, '') })
   const keys = list.keys.map((item) => {
@@ -31,30 +56,39 @@ async function listLinks(namespace) {
   return new Response(html, { headers: { 'Content-Type': 'text/html' } })
 }
 
-async function redirectToLink(namespace, name, origin) {
-  const link = await golinks.get(encodeKey(namespace, name))
-  return Response.redirect(link ?? origin)
-}
 
-async function setLink(namespace, name, body) {
-  await golinks.put(encodeKey(namespace, name), body)
-  return new Response(`set link ${namespace}/${name} to ${body}`)
-}
-
-function parsePath(path) {
-  const parsed = { namespace: null, name: null }
-  if (!path.startsWith('/')) return parsed
-  const segments = path.substring(1).split('/')
-  parsed.namespace = segments[0]
-  if (segments.length === 2) {
-    parsed.name = segments[1]
+function parsePath(url) {
+  console.log("parsing ", url);
+  const [namespace, name] = url.pathname.substring(1).split('/')
+  if (namespace) {
+    return { namespace, name };
   }
-  return parsed
+  return null;
 }
 
-function decodeKey(namespace, key) {
-  return key.substring(`links/${namespace}/`.length)
-}
 function encodeKey(namespace, path) {
   return `links/${namespace}/${path}`
 }
+function decodeKey(namespace, raw) {
+  return raw.substring(`links/${namespace}/`.length)
+}
+
+function encodeLink(namespace, link, key) {
+  const hash = hashString(link);
+  return `keys/${namespace}/${hash}/${key}`;
+}
+function decodeLink(namespace, link, raw) {
+  const hash = hashString(link);
+  return raw.substring(`keys/${namespace}/${hash}/`.length);
+}
+
+function hashString(input) {
+  let hash = 0;
+  for (let i = 0; i < input.length; i++) {
+    const chr = input.charCodeAt(i);
+    hash = ((hash << 5) - hash) + chr;
+    hash |= 0; // Convert to 32bit integer
+  }
+  return hash;
+}
+
