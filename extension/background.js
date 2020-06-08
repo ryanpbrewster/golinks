88ce96f4db8d42e1f60d0b5e541a9f8f18dc1b86
Ryan P. Brewster <RyanPBrewster@gmail.com>
2020-06-07 18:33:41 -0700

    store the namespace in sync storage

--- extension/background.js
+++ extension/background.js
@@ -1,5 +1,11 @@
 const BASE = "https://golinks.brewster.workers.dev";
+
 let namespace = "default";
+chrome.storage.onChanged.addListener((changes, area) => {
+  if (changes.namespace) {
+    namespace = changes.namespace.newValue;
+  }
+});
 
 chrome.webRequest.onBeforeRequest.addListener((details) => {
   const url = new URL(details.url);
@@ -14,16 +20,42 @@ chrome.webRequest.onBeforeRequest.addListener((details) => {
 
 chrome.extension.onConnect.addListener((port) => {
   port.onMessage.addListener((raw) => {
-    const key = JSON.parse(raw).key;
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      const link = tabs[0].url;
-      const put = fetch(BASE + '/' + namespace + '/' + key, {
-        method: "PUT",
-        body: link,
-      });
-      port.postMessage('inflight');
-      put.then(() => port.postMessage('success')).catch(() => port.postMessage('failure'));
-    });
+    const msg = JSON.parse(raw);
+    console.log("handling message: ", msg);
+    let resp;
+    if (msg.setLink) {
+      console.log("setting key ", msg.setLink);
+      resp = handleSetLink(msg.setLink.key);
+    } else if (msg.setNamespace) {
+      console.log("setting namespace ", msg.setNamespace);
+      resp = handleSetNamespace(msg.setNamespace.namespace);
+    } else {
+      resp = Promise.reject();
+    }
+    port.postMessage('inflight');
+    resp
+      .then(() => port.postMessage('success'))
+      .catch(() => port.postMessage('failure'));
   });
 });
 
+async function handleSetNamespace(namespace) {
+  return new Promise((resolve, reject) => {
+    chrome.storage.sync.set({ namespace }, () => {
+      resolve();
+    });
+  });
+}
+
+async function handleSetLink(key) {
+  return new Promise((resolve, reject) => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      const link = tabs[0].url;
+      const request = fetch(BASE + '/' + namespace + '/' + key, {
+        method: "PUT",
+        body: link,
+      });
+      resolve(request);
+    });
+  });
+}
--- extension/popup.js
+++ extension/popup.js
@@ -3,8 +3,8 @@ document.addEventListener('DOMContentLoaded', () => {
   input.addEventListener("keydown", (evt) => {
     if (evt.key === "Enter") {
       input.disabled = true;
-      const port = chrome.extension.connect({ name: "Sample Communication" });
-      port.postMessage(JSON.stringify({ key: input.value }));
+      const port = chrome.extension.connect({ name: "Set Link" });
+      port.postMessage(JSON.stringify({ setLink: { key: input.value } }));
       port.onMessage.addListener((msg) => {
         input.classList = [msg];
       });
